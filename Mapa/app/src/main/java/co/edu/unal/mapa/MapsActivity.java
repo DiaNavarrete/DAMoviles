e00fe5f01c3d01ec3ac356ca91812e767990492a
Diana <dcnavarreter@unal.edu.co>
2019-11-20 23:01:15 -0500

    markers

--- Mapa/app/src/main/java/co/edu/unal/mapa/MapsActivity.java
+++ Mapa/app/src/main/java/co/edu/unal/mapa/MapsActivity.java
@@ -8,33 +8,57 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private Location currentLocation;
     private FusedLocationProviderClient fusedLocationProviderClient;
+    private PlacesClient placesClient;
     private static final int REQUEST_CODE = 101;
+    private Marker baseMarker;
+    private Circle circle;
+    private int km;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        km=1;
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         fetchLastLocation();
-
+        Places.initialize(getApplicationContext(), "AIzaSyDYb7MLGkYxBoFGXd66IwVCRypI_EDH88E");
+        placesClient = Places.createClient(this);
+        setNearbyPlaces();
     }
 
     private void fetchLastLocation() {
@@ -63,22 +87,86 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+    private void setNearbyPlaces(){
+        List<Place.Field> placeFields= new ArrayList<>();
+        placeFields.add(Place.Field.NAME);
+        placeFields.add(Place.Field.LAT_LNG);
+        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.newInstance(placeFields);
+        Task<FindCurrentPlaceResponse> placeResponse = placesClient.findCurrentPlace(request);
+        placeResponse.addOnCompleteListener(new OnCompleteListener<FindCurrentPlaceResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<FindCurrentPlaceResponse> task) {
+                if (task.isSuccessful()) {
+                    FindCurrentPlaceResponse response = task.getResult();
+                    for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
+                        Place p = placeLikelihood.getPlace();
+                        mMap.addMarker(new MarkerOptions().position(p.getLatLng()).title(p.getName()));
+                        Log.i("MAPI", String.format("Place '%s' has likelihood: %f",
+                                placeLikelihood.getPlace().getName(),
+                                placeLikelihood.getLikelihood()));
+                    }
+                } else {
+                    Exception exception = task.getException();
+                    if (exception instanceof ApiException) {
+                        ApiException apiException = (ApiException) exception;
+                        Log.e("MAPI", "Place not found: " + apiException.getStatusCode());
+                    }
+                }
+            }
+        });
+    }
+
+
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        googleMap.getUiSettings().setZoomGesturesEnabled(true);
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
 
-        LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+        final LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
 
-        mMap.addMarker(new MarkerOptions().position(latLng).title("I am here"));
        // mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
        // mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        baseMarker=mMap.addMarker(new MarkerOptions().position(latLng)
+                                            .draggable(true)
+                                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+        circle = mMap.addCircle(new CircleOptions()
+                .center(latLng)
+                .radius(km*1000));
+               // .strokeColor(Color.RED)
+                ///.fillColor(Color.BLUE));
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {}
 
+            @Override
+            public void onMarkerDrag(Marker marker) {  }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                if (marker.equals(baseMarker)) {
+                    LatLng ll=marker.getPosition();
+                    Toast.makeText(MapsActivity.this, ll.latitude + " " +ll.longitude, Toast.LENGTH_SHORT).show();
+                    circle.setCenter(ll);
+                    //PutPlaces();
+                }
+
+            }
+        });
+        //.icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow)));
         mMap.setMyLocationEnabled(true);
+        Log.e("MAPI", "My location");
     }
+    private void PutPlaces(List<Place> places){
+        for(Place p:places)
+            mMap.addMarker(new MarkerOptions().position(p.getLatLng()).title(p.getName()));
+
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
